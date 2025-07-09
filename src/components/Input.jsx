7d7616bc0c0ade58d94951ebe5d6eae0729ad981
Abhishek Tomar <abhishek.tomar02@nagarro.com>
2025-07-10 01:32:16 +0530

    Add the recent feed to homepage

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -80,7 +80,7 @@ export default function Input() {
     setText("");
     setSelectedFile(null);
     setImageFileUrl(null);
-    // location.reload();
+    location.reload();
   };
   return (
     <div className="flex border-b border-gray-200 p-3 space-x-3 w-full">
