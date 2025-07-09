821086dd6bc1da3faa06ead6e8520ee226e2367c
Abhishek Tomar <abhishek.tomar02@nagarro.com>
2025-07-10 00:41:45 +0530

    Complete post image functionality

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -9,6 +9,8 @@ export default function Input() {
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [selectedFile, setSelectedFile] = useState(null);
   const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [text, setText] = useState("");
+  const [postLoading, setPostLoading] = useState(false);
   const imagePickRef = useRef(null);
 
   if (!isSignedIn || !isLoaded) {
@@ -58,7 +60,28 @@ export default function Input() {
       setImageFileUploading(false);
     }
   };
-
+  const handleSubmit = async () => {
+    setPostLoading(true);
+    const response = await fetch("/api/post/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userMongoId: user.publicMetadata.userMongoId,
+        name: user.fullName,
+        username: user.username,
+        text,
+        profileImg: user.imageUrl,
+        image: imageFileUrl,
+      }),
+    });
+    setPostLoading(false);
+    setText("");
+    setSelectedFile(null);
+    setImageFileUrl(null);
+    // location.reload();
+  };
   return (
     <div className="flex border-b border-gray-200 p-3 space-x-3 w-full">
       <img
@@ -71,6 +94,8 @@ export default function Input() {
           className="w-full border-none outline-none tracking-wide min-h-[50px] text-gray-700"
           placeholder="Whats happening"
           rows="2"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         ></textarea>
 
         {selectedFile && (
@@ -100,8 +125,9 @@ export default function Input() {
             onChange={addImageToPost}
           />
           <button
-            disabled
+            disabled={text.trim() === "" || postLoading || imageFileUploading}
             className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
+            onClick={handleSubmit}
           >
             Post
           </button>
